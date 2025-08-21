52ef88e93598ff2f5e8d5fcbcc0e75f2fec6dcf2
Walentalien <voliinyk@student.agh.edu.pl>
2025-08-21 15:27:03 +0200

    Update defaults to comply with proxy/middleware/version

--- version/version.go
+++ version/version.go
@@ -1,10 +1,10 @@
 package version
 
 // Version of the binary. Set at build time using -ldflags.
-var Version = ""
+var Version = "unknown"
 
 // CommitHash : short version of git commit hash of the source code. Set at build time using -ldflags.
-var CommitHash = ""
+var CommitHash = "unknown"
 
 // HeaderCLIVersion is the HTTP header used to identify CLI versions.
 const HeaderCLIVersion = "X-CLI-Version"
