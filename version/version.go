e06fc9fdee6813d035edf6ebc805fa00e0c3f599
Walentalien <voliinyk@student.agh.edu.pl>
2025-08-21 15:04:13 +0200

    Added shared http header constant

--- version/version.go
+++ version/version.go
@@ -5,3 +5,6 @@ var Version = ""
 
 // CommitHash : short version of git commit hash of the source code. Set at build time using -ldflags.
 var CommitHash = ""
+
+// HeaderCLIVersion is the HTTP header used to identify CLI versions.
+const HeaderCLIVersion = "X-CLI-Version"
