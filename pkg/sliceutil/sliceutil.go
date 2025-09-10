0041ac354c2de998a668a50cc85efb9ebbc5041b
Walentalien <voliinyk@student.agh.edu.pl>
2025-09-10 23:11:50 +0200

    added improved slice processing util

---
+++ pkg/sliceutil/sliceutil.go
@@ -0,0 +1,9 @@
+package sliceutil
+
+func ConvertSlice[T, U any](src []T, converter func(T) U) []U {
+	result := make([]U, len(src)) // preallocate with exact length
+	for i, v := range src {
+		result[i] = converter(v) // assign directly, no append
+	}
+	return result
+}
---
+++ pkg/sliceutil/sliceutil_test.go
@@ -0,0 +1,39 @@
+package sliceutil_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/getoptimum/optimum-common/pkg/sliceutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConvertSlice2(t *testing.T) {
+	t.Parallel()
+
+	require.Equal(t,
+		[]string{"Number: 1", "Number: 2", "Number: 3"},
+		sliceutil.ConvertSlice([]int{1, 2, 3}, func(i int) string {
+			return fmt.Sprintf("Number: %d", i)
+		}),
+	)
+
+	require.Equal(t,
+		[]int{2, 4, 6},
+		sliceutil.ConvertSlice([]float32{1, 2, 3}, func(i float32) int {
+			return int(i) * 2
+		}),
+	)
+
+	// Edge cases:
+	require.Equal(t,
+		[]int{}, // empty slice (not nil)
+		sliceutil.ConvertSlice([]int{}, func(i int) int { return i }),
+	)
+
+	var nilSrc []int
+	require.Equal(t,
+		[]int{}, // your impl returns empty, not nil
+		sliceutil.ConvertSlice(nilSrc, func(i int) int { return i }),
+	)
+}
