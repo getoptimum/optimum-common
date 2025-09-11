d58a9ff091d9273d1794a2b491ba71efa4355020
Walentalien <voliinyk@student.agh.edu.pl>
2025-09-11 23:41:37 +0200

    move tests to test module

--- pkg/utils/rand_test.go
+++ pkg/utils/rand_test.go
@@ -1,15 +1,16 @@
-package utils
+package utils_test
 
 import (
 	"testing"
 
+	randutil "github.com/getoptimum/optimum-common/pkg/utils"
 	"github.com/stretchr/testify/require"
 )
 
 func TestRandInt64(t *testing.T) {
 	matches := map[int64]struct{}{}
 	for i := 0; i < 1_000_000; i++ {
-		val := RandInt64()
+		val := randutil.RandInt64()
 		_, ok := matches[val]
 		require.False(t, ok)
 		matches[val] = struct{}{}
@@ -20,7 +21,7 @@ func TestRandBetween(t *testing.T) {
 	minimum := 10
 	maximum := 20
 	for i := 0; i < 1_000_000; i++ {
-		val := RandBetween(minimum, maximum)
+		val := randutil.RandBetween(minimum, maximum)
 		require.True(t, val >= minimum && val < maximum, val)
 	}
 }
