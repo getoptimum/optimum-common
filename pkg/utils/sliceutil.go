e672afa19f5831c76eff3909b004c0a4730fbb44
Walentyn <163281616+Walentalien@users.noreply.github.com>
2025-09-11 13:31:33 +0200

    Pkg/slices (#17)
    * added improved slice processing util
    
    * chore: go mod tidy
    
    * delete unnessesary comments
    
    * move slice utils to utils folder
    
    * renamed ConvertSlice -> MapSlice
    
    * move sliceutils to util folder

---
+++ pkg/utils/sliceutil.go
@@ -0,0 +1,9 @@
+package utils
+
+func MapSlice[T, U any](src []T, converter func(T) U) []U {
+	result := make([]U, len(src)) // preallocate with exact length
+	for i, v := range src {
+		result[i] = converter(v)
+	}
+	return result
+}
---
+++ pkg/utils/sliceutil_test.go
@@ -0,0 +1,39 @@
+package utils_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/getoptimum/optimum-common/pkg/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConvertSlice2(t *testing.T) {
+	t.Parallel()
+
+	require.Equal(t,
+		[]string{"Number: 1", "Number: 2", "Number: 3"},
+		utils.MapSlice([]int{1, 2, 3}, func(i int) string {
+			return fmt.Sprintf("Number: %d", i)
+		}),
+	)
+
+	require.Equal(t,
+		[]int{2, 4, 6},
+		utils.MapSlice([]float32{1, 2, 3}, func(i float32) int {
+			return int(i) * 2
+		}),
+	)
+
+	// Edge cases:
+	require.Equal(t,
+		[]int{}, // empty slice (not nil)
+		utils.MapSlice([]int{}, func(i int) int { return i }),
+	)
+
+	var nilSrc []int
+	require.Equal(t,
+		[]int{},
+		utils.MapSlice(nilSrc, func(i int) int { return i }),
+	)
+}
