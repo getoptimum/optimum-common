597610f8059f5eaa4c29b138bcc523f18711b157
Walentalien <voliinyk@student.agh.edu.pl>
2025-09-26 12:54:35 +0200

    injecting version and commit hash

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/getoptimum/optimum-common/pkg/version"
-	"github.com/libp2p/go-libp2p/core/peer"
 )
 
 // LogMode defines logging verbosity levels.
@@ -62,6 +61,7 @@ func NewAppSLogger(appHash string, mode LogMode, fields ...Field) AppLogger {
 	return InitLogger([]io.Writer{os.Stdout}, appHash, validateLogMode(mode), fields...)
 }
 
+// TODO: appHash should be removed
 // InitLogger initializes a multi-output JSON logger with slog.
 func InitLogger(writers []io.Writer, appHash string, mode LogMode, fields ...Field) AppLogger {
 	logs := make([]*slog.Logger, 0, len(writers))
@@ -83,15 +83,11 @@ func InitLogger(writers []io.Writer, appHash string, mode LogMode, fields ...Fie
 			},
 		})
 		attrs := make([]any, 0, len(fields)+2)
-		if appHash != "" && len(appHash) > 6 {
-			attrs = append(attrs, slog.String("version", appHash[:6]))
-		}
 		for _, f := range fields {
 			attrs = append(attrs, f.slog())
 		}
-		if gitHash := version.GetCommitHash(); gitHash != "" {
-			attrs = append(attrs, slog.String("commit", gitHash))
-		}
+		attrs = append(attrs, slog.String("commit", version.GetCommitHash()))
+		attrs = append(attrs, slog.String("version", version.GetVersion()))
 
 		lw := slog.New(handler).With(attrs...)
 		logs = append(logs, lw)
@@ -178,74 +174,4 @@ func logLevelFromMode(mode LogMode) slog.Level {
 	default:
 		return slog.LevelInfo
 	}
-}
-
-// --- Attribute helpers ---
-
-func WithAny(key string, val any) Field {
-	return Field{key: key, value: val}
-}
-
-func WithString(key, val string) Field {
-	return Field{key: key, value: val}
-}
-
-func WithUint64(key string, val uint64) Field {
-	return Field{key: key, value: val}
-}
-
-func WithBool(key string, val bool) Field {
-	return Field{key: key, value: val}
-}
-
-func WithInt64(key string, val int64) Field {
-	return Field{key: key, value: val}
-}
-
-func WithInt(key string, val int) Field {
-	return Field{key: key, value: val}
-}
-
-// --- Generic helpers ---
-
-func WithModule(val string) *Field {
-	f := WithString("module", val)
-	return &f
-}
-
-func WithError(err error) *Field {
-	f := WithString("err", err.Error())
-	return &f
-}
-
-func WithFilePath(filePath string) Field {
-	return WithString("file", filePath)
-}
-
-func WithService(serviceName string) Field {
-	return WithString("service", serviceName)
-}
-
-// WithPeer adds the peer ID of the given AddrInfo.
-func WithPeer(info peer.AddrInfo) Field {
-	return WithString("peer", info.ID.String())
-}
-
-// WithPeerAddrs adds comma separated multiaddrs of the given peer.
-func WithPeerAddrs(info peer.AddrInfo) Field {
-	addrs := make([]string, 0, len(info.Addrs))
-	for _, addr := range info.Addrs {
-		addrs = append(addrs, addr.String())
-	}
-	return WithString("peer", strings.Join(addrs, ","))
-}
-
-// WithTopic adds a pubsub topic as a string field.
-func WithTopic(topic string) Field {
-	return WithString("topic", topic)
-}
-
-// WithTopicBytes adds a topic represented as byte slice.
-func WithTopicBytes(topic []byte) Field {
-	return WithString("topic", string(topic))
 }
