33d5d45a211ae8c92ed8d81ce15cbb08067eb20a
Walentalien <voliinyk@student.agh.edu.pl>
2025-09-29 10:51:32 +0200

    table driven, nil tests for all types + bool

--- pkg/utils/pointers_test.go
+++ pkg/utils/pointers_test.go
@@ -8,24 +8,81 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPointers(t *testing.T) {
+func TestToAndFromPointer(t *testing.T) {
+	type testCase[T comparable] struct {
+		name     string
+		input    T
+		isNil    bool
+		expected T
+	}
+
 	t.Run("string", func(t *testing.T) {
 		str := uuid.NewString()
-		strPointer := utils.ToPointer(str)
-		require.Equal(t, str, utils.FromPointer(strPointer))
+		tests := []testCase[string]{
+			{"non-nil string", str, false, str},
+			{"nil string", "", true, ""},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				var ptr *string
+				if !tt.isNil {
+					ptr = utils.ToPointer(tt.input)
+				}
+				result := utils.FromPointer(ptr)
+				require.Equal(t, tt.expected, result)
+			})
+		}
 	})
+
 	t.Run("int", func(t *testing.T) {
-		integerVal := 123
-		integerPointer := utils.ToPointer(integerVal)
-		require.Equal(t, integerVal, utils.FromPointer(integerPointer))
+		tests := []testCase[int]{
+			{"non-nil int", 123, false, 123},
+			{"nil int", 0, true, 0},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				var ptr *int
+				if !tt.isNil {
+					ptr = utils.ToPointer(tt.input)
+				}
+				result := utils.FromPointer(ptr)
+				require.Equal(t, tt.expected, result)
+			})
+		}
 	})
+
 	t.Run("float", func(t *testing.T) {
-		floatVal := 123.123
-		floatPointer := utils.ToPointer(floatVal)
-		require.Equal(t, floatVal, utils.FromPointer(floatPointer))
+		tests := []testCase[float64]{
+			{"non-nil float", 123.123, false, 123.123},
+			{"nil float", 0.0, true, 0.0},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				var ptr *float64
+				if !tt.isNil {
+					ptr = utils.ToPointer(tt.input)
+				}
+				result := utils.FromPointer(ptr)
+				require.Equal(t, tt.expected, result)
+			})
+		}
 	})
-	t.Run("nil", func(t *testing.T) {
-		var sample *string
-		require.Equal(t, "", utils.FromPointer(sample))
+
+	t.Run("bool", func(t *testing.T) {
+		tests := []testCase[bool]{
+			{"true bool", true, false, true},
+			{"false bool", false, false, false},
+			{"nil bool", false, true, false},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				var ptr *bool
+				if !tt.isNil {
+					ptr = utils.ToPointer(tt.input)
+				}
+				result := utils.FromPointer(ptr)
+				require.Equal(t, tt.expected, result)
+			})
+		}
 	})
 }
