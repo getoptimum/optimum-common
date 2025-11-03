d65d23f9e03f9eebf8a1854dd62028b5d65fee5c
Alejandro <abergasov@gmail.com>
2025-11-03 13:20:29 +0100

    rename field

--- pkg/entities/dynamic_config.go
+++ pkg/entities/dynamic_config.go
@@ -7,9 +7,9 @@ type DynamicConfig struct {
 	ClusterID       string    `json:"cluster_id"`
 	UpdatedAt       time.Time `json:"updated_at"`
 	EnableABTesting bool      `json:"enable_ab_testing"`
-	// SkipMessageFromSelf is a flag that indicates whether messages originating from the node itself should be ignored.
+	// ExcludeSelfMessages is a flag that indicates whether messages originating from the node itself should be ignored.
 	// used for tracking eth latency measurements
-	SkipMessageFromSelf bool `json:"skip_message_from_self"`
+	ExcludeSelfMessages bool `json:"exclude_self_messages"`
 
 	// RLNC and message settings
 	RandomMessageSize        int64   `json:"random_message_size_bytes"`
