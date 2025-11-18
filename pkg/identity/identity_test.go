7be1bf28ca8ac1c86ec13a64c7cf9d90b763f788
Alejandro <abergasov@gmail.com>
2025-11-18 17:24:45 +0100

    identity code added

---
+++ pkg/identity/identity_test.go
@@ -0,0 +1,43 @@
+package identity_test
+
+import (
+	"testing"
+
+	"github.com/getoptimum/optimum-common/pkg/identity"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPersistedIdentity(t *testing.T) {
+	t.Run("default key type", func(t *testing.T) {
+		dir := t.TempDir()
+		_, err := identity.EnsureIdentity(dir)
+		require.NoError(t, err)
+
+		info, err := identity.ExtractIdentityFromDir(dir)
+		require.NoError(t, err)
+		t.Log(info.ID.String())
+
+		key, err := crypto.UnmarshalPrivateKey(info.Key)
+		require.NoError(t, err)
+		id, err := peer.IDFromPrivateKey(key)
+		require.NoError(t, err)
+		require.Equal(t, id, info.ID)
+	})
+	t.Run("specified key type", func(t *testing.T) {
+		dir := t.TempDir()
+		_, err := identity.EnsureIdentity(dir, identity.GenerateIdentitySecp256k1)
+		require.NoError(t, err)
+
+		info, err := identity.ExtractIdentityFromDir(dir)
+		require.NoError(t, err)
+		t.Log(info.ID.String())
+
+		key, err := crypto.UnmarshalPrivateKey(info.Key)
+		require.NoError(t, err)
+		id, err := peer.IDFromPrivateKey(key)
+		require.NoError(t, err)
+		require.Equal(t, id, info.ID)
+	})
+}
