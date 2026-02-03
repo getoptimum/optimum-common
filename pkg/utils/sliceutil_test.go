c86bb491de8c5b810c8df69617c2addb2b2203df
swarnabhasinha <swarnabhasinha@gmail.com>
2026-02-03 19:05:27 +0530

    fix

--- pkg/utils/sliceutil_test.go
+++ pkg/utils/sliceutil_test.go
@@ -144,18 +144,24 @@ func TestExcludeFromSlice(t *testing.T) {
 func TestChunkSlice(t *testing.T) {
 	t.Parallel()
 
-	// Exact division
-	require.Equal(t, [][]uint64{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, utils.ChunkSlice([]uint64{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3))
+	tests := []struct {
+		name      string
+		slice     []int
+		chunkSize int
+		want      [][]int
+	}{
+		{"exact division", []int{1, 2, 3, 4, 5, 6}, 2, [][]int{{1, 2}, {3, 4}, {5, 6}}},
+		{"remainder", []int{1, 2, 3, 4, 5, 6, 7, 8}, 3, [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8}}},
+		{"single chunk", []int{1, 2, 3}, 5, [][]int{{1, 2, 3}}},
+		{"empty slice", []int{}, 3, [][]int{}},
+		{"chunk size larger than slice", []int{1, 2}, 10, [][]int{{1, 2}}},
+	}
 
-	// Remainder
-	require.Equal(t, [][]uint64{{1, 2, 3}, {4, 5, 6}, {7, 8}}, utils.ChunkSlice([]uint64{1, 2, 3, 4, 5, 6, 7, 8}, 3))
-
-	// Single chunk
-	require.Equal(t, [][]int{{1, 2, 3}}, utils.ChunkSlice([]int{1, 2, 3}, 5))
-
-	// Empty slice
-	require.Equal(t, [][]string{}, utils.ChunkSlice([]string{}, 3))
-
-	// Chunk size larger than slice
-	require.Equal(t, [][]string{{"a", "b"}}, utils.ChunkSlice([]string{"a", "b"}, 10))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := utils.ChunkSlice(tt.slice, tt.chunkSize)
+			require.Equal(t, tt.want, got)
+		})
+	}
 }
