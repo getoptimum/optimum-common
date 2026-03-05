a306f3eeccb8c6307d06821ddc4d69f55305dd76
Michael Meier <m.wiresurfer@gmail.com>
2026-03-05 12:26:26 -0500

    chore: add config builder functions

--- pkg/rlnc/rlnc.go
+++ pkg/rlnc/rlnc.go
@@ -20,8 +20,45 @@ type EncoderConfig struct {
 	PrepareData  bool // If true, prepare data for coding
 }
 
+func WithSystematic(systematic bool) Option {
+	return func(cfg *EncoderConfig) {
+		cfg.Systematic = systematic
+	}
+}
+
+func WithNumThreads(numThreads int) Option {
+	return func(cfg *EncoderConfig) {
+		cfg.NumThreads = numThreads
+	}
+}
+
+func WithNumShards(numShards int) Option {
+	return func(cfg *EncoderConfig) {
+		cfg.NumShards = numShards
+	}
+}
+
+func WithShredFactor(shredFactor int) Option {
+	return func(cfg *EncoderConfig) {
+		cfg.ShredFactor = shredFactor
+	}
+}
+
+func WithShredMaxSize(maxSize int) Option {
+	return func(cfg *EncoderConfig) {
+		cfg.ShredMaxSize = maxSize
+	}
+}
+
+func WithPrepareData(prepareData bool) Option {
+	return func(cfg *EncoderConfig) {
+		cfg.PrepareData = prepareData
+	}
+}
+
 type EncodeIntoShards = func(context.Context, []byte, ...Option) ([]*Shard, error)
-type StreamShards = func() (chan *Shard, error)
+type StreamShards = func(context.Context, []byte, ...Option) (chan *Shard, error)
+type NewShardSet = func(numCoefficients, shardLength int) ShardSet
 
 type ShardSet interface {
 	Add(coefficients, data []byte) error
