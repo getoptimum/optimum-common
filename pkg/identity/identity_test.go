974130f10f6b5d573d3832826a7d64667ea01892
Swarna <swarnabhasinha@gmail.com>
2026-06-05 18:29:35 +0530

    test: assert secp256k1 default CL identity key type (#139)

--- pkg/identity/identity_test.go
+++ pkg/identity/identity_test.go
@@ -1,6 +1,7 @@
 package identity_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/getoptimum/optimum-common/pkg/identity"
@@ -12,12 +13,13 @@ import (
 func TestPersistedIdentity(t *testing.T) {
 	t.Run("default key type", func(t *testing.T) {
 		dir := t.TempDir()
-		_, err := identity.EnsureIdentity(dir)
+		pk, err := identity.EnsureIdentity(dir)
 		require.NoError(t, err)
+		require.EqualValues(t, crypto.Secp256k1, pk.Type())
 
 		info, err := identity.ExtractIdentityFromDir(dir)
 		require.NoError(t, err)
-		t.Log(info.ID.String())
+		require.True(t, strings.HasPrefix(info.ID.String(), "16Uiu"))
 
 		key, err := crypto.UnmarshalPrivateKey(info.Key)
 		require.NoError(t, err)
@@ -27,17 +29,34 @@ func TestPersistedIdentity(t *testing.T) {
 	})
 	t.Run("specified key type", func(t *testing.T) {
 		dir := t.TempDir()
-		_, err := identity.EnsureIdentity(dir, identity.GenIdentityEd25519)
+		pk, err := identity.EnsureIdentity(dir, identity.GenIdentityEd25519)
 		require.NoError(t, err)
+		require.EqualValues(t, crypto.Ed25519, pk.Type())
 
 		info, err := identity.ExtractIdentityFromDir(dir)
 		require.NoError(t, err)
-		t.Log(info.ID.String())
+		require.True(t, strings.HasPrefix(info.ID.String(), "12D3Koo"))
 
 		key, err := crypto.UnmarshalPrivateKey(info.Key)
 		require.NoError(t, err)
 		id, err := peer.IDFromPrivateKey(key)
 		require.NoError(t, err)
 		require.Equal(t, id, info.ID)
+	})
+	t.Run("existing key is loaded not regenerated", func(t *testing.T) {
+		dir := t.TempDir()
+		_, err := identity.EnsureIdentity(dir, identity.GenerateIdentitySecp256k1)
+		require.NoError(t, err)
+
+		info, err := identity.ExtractIdentityFromDir(dir)
+		require.NoError(t, err)
+
+		pk, err := identity.EnsureIdentity(dir, identity.GenIdentityEd25519)
+		require.NoError(t, err)
+		require.EqualValues(t, crypto.Secp256k1, pk.Type())
+
+		id, err := peer.IDFromPrivateKey(pk)
+		require.NoError(t, err)
+		require.Equal(t, info.ID, id)
 	})
 }
